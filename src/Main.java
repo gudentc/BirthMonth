f5503fe28b813416924a30c5b095160139169ee4
tguden/Booleans <gudentc@mail.uc.edu>
2023-06-11 13:51:47 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        // Declare Variables
+        int birthMonth = 0;
+        String trash = "";
+
+        // Get input and test
+        System.out.print("\nWhat is your birth month [1 - 12]: ");
+        if (in.hasNextInt())
+        {
+            birthMonth = in.nextInt();
+            in.nextLine(); // Clears the buffer
+        }
+        else
+        {
+            trash = in.nextLine();
+            System.out.println("\nYou must enter a number 1 - 12, not " + trash);
+            System.out.println("Rerun the program!");
+            System.exit(0);
+        }
+        // We make it here we have an integer.
+        if (birthMonth >= 1 && birthMonth <=12) {
+            System.out.println("\nYour Birth Month is: " + birthMonth);
+        }
+        else {
+            System.out.println("You entered and invalid month variable: " + birthMonth);
+        }
+
+    }
+}
